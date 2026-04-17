6a62b48e0ad416f12e018df679aab1c814d515f5
andriihrechko <andriihrechko250703@gmail.com>
2026-04-17 10:49:43 +0300

    style and test: add some tests and fix style

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -20,7 +20,7 @@ def test_sum_of_the_parts_should_be_equal_to_value(
     (131, 12),
     (10, 9)
 ])
-def test_should_split_into_equal_parts_when_value_divisible_by_parts(
+def test_length_of_result_must_be_equal_parts(
         value: int,
         parts: int,
 ) -> None:
@@ -45,8 +45,7 @@ def test_parts_should_be_sorted_when_they_are_not_equal(
         value: int,
         parts: int
 ) -> None:
-    actual = split_integer(value, parts)
-    assert actual == sorted(actual)
+    assert split_integer(value, parts) == sorted(split_integer(value, parts))
 
 
 @pytest.mark.parametrize("value,parts", [
@@ -58,6 +57,18 @@ def test_should_add_zeros_when_value_is_less_than_number_of_parts(
         value: int,
         parts: int
 ) -> None:
-    actual = split_integer(value, parts)
     expected_nulls_count = parts - value
-    assert actual.count(0) == expected_nulls_count
+    assert split_integer(value, parts).count(0) == expected_nulls_count
+
+
+@pytest.mark.parametrize("value,parts", [
+    (4, 5),
+    (0, 10),
+    (12, 24)
+])
+def test_should_add_zeros_when_value_is_less_than_number_of_parts(
+        value: int,
+        parts: int
+) -> None:
+    value = split_integer(value, parts)
+    assert max(value) - min(value) <= 1
